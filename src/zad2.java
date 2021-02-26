37ee6f9e8a04b997398ae71937e7f246b0ddc4af
Karolina <karkarolina87@gmail.com>
2021-02-26 08:38:56 +0100

    zad2 - Napisz program, w którym zainicjalizujesz zmienne typu prymitywnego i przypiszesz do nich następujące wartości: byte -> b = 68 short -> s = 10000 int -> i = 2147483647 long -> l = 822337203 float -> f = 234.5f double -> d = 123.4 boolean -> bo = true string -> st = "Software Development Academy" Wartości zmiennych wydrukuj na standardowe wyjście (ekran).

---
+++ src/zad2.java
@@ -0,0 +1,21 @@
+public class zad2 {
+    public static void main(String[] args) {
+        byte b = 68;
+        short s = 10000;
+        int i = 2147483647;
+        long l = 822337203;
+        float f = 234.5f;
+        double d = 123.4;
+        boolean bo = true;
+        String st = "Software Development Academy";
+        
+        System.out.println("Zmienna byte " + b);
+        System.out.println("Zmienna short " + s);
+        System.out.println("Zmienna int " + i);
+        System.out.println("Zmienna long " + l);
+        System.out.println("Zmienna float " + f);
+        System.out.println("Zmienna double " + d);
+        System.out.println("Zmienna bolean " + bo);
+        System.out.println("Zmienna string " + st);
+    }
+}
