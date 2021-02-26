c686a5d17da2ea6b467c8a79a60d2504759c0917
Karolina <karkarolina87@gmail.com>
2021-02-26 09:01:31 +0100

    zad4 - Napisz program, który pobierze od użytkownika liczbę całkowitą i wypisze ją na ekran. Na ekranie powinien wyświetlać się nastepujący komunikat "Użytkownik wprowadził X", gdzie X jest liczbą podaną przez użytkownika. Zakładamy sytuację, w której użytkownik, nie będzie wprowadzał innych typów danych niz liczby całkowite.
           Do obsługi wprowadzania danych przez użytkownika zastosuj bibliotekę Scanner.
           Wykonaj testy, sprawdź jak program zachowuje się dla innych typów danych.

--- src/zad3.java
+++ src/zad3.java
@@ -1,11 +1,11 @@
 public class zad3 {
     public static void main(String[] args) {
-        int i = 100000000000000000000000;
-        long l = -900000000000000000000000;
+        // int i = 100000000000000000000000;
+        // long l = -900000000000000000000000;
         double d = 100;
 
-        System.out.println("Zmienna int " + i); // integer number too large
-        System.out.println("Zmienna long " + l); // integer number too large
+        //System.out.println("Zmienna int " + i); // integer number too large
+        //System.out.println("Zmienna long " + l); // integer number too large
         System.out.println("Zmienna double " + d);
     }
 }
