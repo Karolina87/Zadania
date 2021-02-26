cc72a1dfc828b41dcdf82cd615c49fe68f4ed7b1
Karolina <karkarolina87@gmail.com>
2021-02-26 09:00:51 +0100

    zad4 - Napisz program, który pobierze od użytkownika liczbę całkowitą i wypisze ją na ekran. Na ekranie powinien wyświetlać się nastepujący komunikat "Użytkownik wprowadził X", gdzie X jest liczbą podaną przez użytkownika. Zakładamy sytuację, w której użytkownik, nie będzie wprowadzał innych typów danych niz liczby całkowite.
           Do obsługi wprowadzania danych przez użytkownika zastosuj bibliotekę Scanner.
           Wykonaj testy, sprawdź jak program zachowuje się dla innych typów danych.

---
+++ src/zad4.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class zad4 {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Podaj liczbę całkowitą");
+        int a = userInput.nextInt();
+        System.out.println("Podałeś liczbę: " + a);
+    }
+}
